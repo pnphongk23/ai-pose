d5f87866e61e2760f9afa09583204bff474c3f64
phongpn <phongpn.k23dtcn385@stu.ptit.edu.vn>
2026-05-02 12:30:57 +0700

    build: add Ktor deps and kotlinx-datetime to shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.sqldelight)
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 kotlin {
@@ -14,9 +15,20 @@ kotlin {
             implementation(libs.sqldelight.runtime)
             implementation(libs.sqldelight.coroutines)
             implementation(libs.coroutines.core)
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
         }
         iosMain.dependencies {
             implementation(libs.sqldelight.native)
+            implementation(libs.ktor.client.darwin)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependencies {
+                implementation(libs.ktor.client.mock)
+            }
         }
     }
 }
